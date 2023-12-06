f0bc5361b3491010bb978a342adea4a29bcf2acf
Jahangir Alam <jahangir12f1993.com>
2023-12-06 13:24:22 +0600

    up

--- api/config.php
+++ api/config.php
@@ -3,5 +3,5 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
-$con = mysqli_connect("localhost","root","","react_project") or die("Connection Undone");
+$con = mysqli_connect("localhost","root","","e_commerce") or die("Connection Undone");
 ?>
